3118dd15724d24ca4692a4d2cedd7108ee539642
Didier Pinto <didieresnaki@hotmail.com>
2024-07-12 21:54:42 -0500

    Actualizacion sobre el proyecto V2

--- app/src/main/java/com/example/ecogarden/menu.java
+++ app/src/main/java/com/example/ecogarden/menu.java
@@ -1,6 +1,12 @@
 package com.example.ecogarden;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +15,30 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class menu extends AppCompatActivity {
+    private TextView cerrarSesion;
+    private TextView resultadoPrecioPatin;
+    private TextView resultadoPrecioAuto;
+    private TextView resultadoPrecioBicleta;
+    private EditText producto1;
+    private EditText producto2;
+    private EditText producto3;
+    private Button calcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu);
+
+        cerrarSesion = findViewById(R.id.textView3);
+
+        cerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent back = new Intent(menu.this, MainActivity.class);
+                startActivity(back);
+            }
+        });
 
     }
 }
